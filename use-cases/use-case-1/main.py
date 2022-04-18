e78dc50f1e6c765ebdce5efc7b05c038688d4a35
Joao Nisa <joao.nisa@MBP-de-Joao.Home>
2022-04-18 09:59:17 +0100

    configuration file with the primary keys and adaptation of the pipeline

--- use-cases/use-case-1/main.py
+++ use-cases/use-case-1/main.py
@@ -14,6 +14,7 @@ from client.ksql import (create_mt_views,
     insert_values
     )
 
+
 # get a ksql client
 client = KSQLAPI('http://ksqldb-server:8088')
 
@@ -25,36 +26,28 @@ source = ['data', '1-raw-data']
 landing = ['data', '2-curated-data']
 data_format = 'JSON'
 
-# map input files: output files
-csv_to_convert = {
-    'event_v2_data.csv': 'event_v2_data.json',
-    'payment_instrument_token_data.csv': 'payment_instrument_token_data.json',
-    'transaction_request.csv': 'transaction_request.json',
-    'transaction.csv': 'transaction.json'
-}
 
-# convertion lifecyle application
-for key in iter(csv_to_convert):
-    recs = csv_to_dict(key, cur_dir, source)
-    json_creator(cur_dir, landing, recs, csv_to_convert[key])
+# configuration files
+conf_path = os.path.join('/'.join(cur_dir.split('/')[:-2]), 'confs')
 
-# data types map (between python and SQL)
-path = cur_dir.split('/')[:-2]
-f = open(os.path.join('/'.join(path), 'confs', 'py_to_kafka.json'))
+f = open(os.path.join(conf_path, 'py_to_kafka.json'))
 map = json.load(f)
 
+c = open(os.path.join(conf_path, 'convertion_map.json'))
+features = json.load(c)
+
+
+# convertion lifecyle application
+for key in iter(features):
+    recs = csv_to_dict(key, cur_dir, source)
+    json_creator(cur_dir, landing, recs, features[key]['output'])
+
 # create the required tables with data
-for t in list(csv_to_convert.keys())[1::]:
+for t in list(features.keys()):
+    cols, dtyp = get_schema(get_sample(os.path.join(cur_dir, '/'.join(landing), features[t]['output'])), map)
+    vals = get_records(os.path.join(cur_dir, '/'.join(landing), features[t]['output']))
+    client.ksql(create_table(t[:-4], cols, dtyp, t[:-4], data_format, 1, features[t]['primary_key']))
 
-    cols, dtyp = get_schema(get_sample(os.path.join(cur_dir, '/'.join(landing), csv_to_convert[t])), map)
-    vals = get_records(os.path.join(cur_dir, '/'.join(landing), csv_to_convert[t]))
-
-    # for test purposes we are forcing the 'token_id' as primary column
-    if 'token_id' in cols:
-        p_key = 'token_id'
-    else: 
-        p_key = cols[0]
-    client.ksql(create_table(t[:-4], cols, dtyp, t[:-4], data_format, 1, p_key))
     for v in vals:
         client.ksql(insert_values(t[:-4], tuple(cols), v))
 
--- utils/disruptor.py
+++ utils/disruptor.py
@@ -1,6 +1,5 @@
 
 
-import pdb
 import json
 
 
